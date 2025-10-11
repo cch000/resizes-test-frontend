d15ad525f4baed41738e423c5fd02588132cb858
Carlos <carloscamposherrera446@gmail.com>
2025-10-11 13:40:10 +0200

    init repo

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    buildInputs = with pkgs; [
+      nodejs
+    ];
+
+    name = "backend";
+    #info-server = pkgs.buildNpmPackage {
+    #  inherit name;
+    #  src = ./.;
+    #  npmDepsHash = "sha256-vR2Ap08SpK95ETfbQA6G945Yt90MlR6cA0oTqahqetg=";
+    #  version = "0.1.0";
+    #  dontNpmBuild = true;
+    #  installPhase = ''
+    #    mkdir -p $out/bin $out/lib
+    #    cp -rv ./node_modules $out/lib
+    #    cp -rv ./src/main.js $out/lib
+    #    cp -rv package.json $out/lib
+    #    cat > $out/bin/${name} << EOF
+    #    #!/bin/sh
+    #    ${pkgs.lib.getExe pkgs.nodejs} $out/lib/main.js
+    #    EOF
+    #    chmod +x $out/bin/${name}
+    #  '';
+    #  meta.mainProgram = "${name}";
+    #};
+  in {
+    #packages.${pkgs.system} = {
+    #  inherit info-server;
+    #  default = info-server;
+    #};
+
+    devShells.${pkgs.system}.default = pkgs.mkShell {
+      inherit buildInputs;
+
+      packages = with pkgs; [
+        nixd #nix language server
+        alejandra
+      ];
+    };
+  };
+}
