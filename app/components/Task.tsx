5797ceec08ca66d774ed8aba59ba02308d1d71fe
cch <85367181+cch000@users.noreply.github.com>
2025-10-12 13:36:39 +0200

    Revert "remove unnecessary code"

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,16 +1,30 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
+import { useDrag } from "react-dnd";
 import styles from "../styles/Kanban.module.css";
 import { TaskProps } from "../types";
 
 const task: React.FC<TaskProps> = ({
   task,
+  index,
   columnId,
   deletetask,
 }) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const [, drag] = useDrag({
+    type: "TASK",
+    item: {
+      id: task.id,
+      sourceColId: columnId,
+      sourceIndex: index,
+    },
+  });
+
+  drag(ref);
 
   return (
-    <div className={styles.task}>
+    <div className={styles.task} ref={ref}>
       <h1>{task.title}</h1>
       <span>{task.description}</span>
       <button onClick={() => deletetask(columnId, task.id)}>✕</button>
